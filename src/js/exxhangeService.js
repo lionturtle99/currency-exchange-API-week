28905900023f9d4869f6d58773ae944616a11a1a
Liam Eller <liamthelastson@gmail.com>
2022-02-11 09:31:03 -0800

    WIP: testing async api function

---
+++ src/js/exxhangeService.js
@@ -0,0 +1,14 @@
+export class exchangeService {
+  static exchangeAPI(base, currency, amount){
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${base}/${currency}/${amount}`)
+    .then(function(response) {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+      
+    }).catch(function(error) {
+      return error;
+    });
+  }
+}
